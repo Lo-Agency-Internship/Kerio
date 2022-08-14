f62953cff535a3ed21a0f6f4b3e5cc5a189473e9
unknown <maryam.pourganji92@gmail.com>
2022-08-14 10:39:47 +0430

    feat: Add side bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,7 @@
 import './App.css';
 
 function App() {
-	return (
-		<div className="App">
-
-		</div>
-	);
+	return <div className="App"></div>;
 }
 
 export default App;
--- frontend/src/components/atoms/button.tsx
+++ frontend/src/components/atoms/button.tsx
@@ -1,6 +1,6 @@
 // export interface Props {}
 
-export function Button() {
+export default function Button() {
 	return (
 		<>
 			<button className="border border-3 border-black p-2 hover:bg-gray-500 rounded hover:text-white"></button>
