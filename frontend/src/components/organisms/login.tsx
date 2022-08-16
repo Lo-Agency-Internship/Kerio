c365dd51766c00c8d2cffc5f0d9e43b85a69ccb2
unknown <maryam.pourganji92@gmail.com>
2022-08-16 16:05:14 +0430

    feat: Add signin modal

---
+++ frontend/src/components/organisms/login.tsx
@@ -0,0 +1,35 @@
+function SignInModal() {
+	return (
+		<div className="flex flex-col items-start justify-between w-full px-10 pt-5 pb-20 lg:pt-20 lg:flex-row">
+			<div className="relative z-10 w-full max-w-2xl mt-20 lg:mt-0 lg:w-5/12">
+				<div className="relative z-10 flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl">
+					<h4 className="w-full text-4xl font-medium leading-snug">LogIn Form</h4>
+					<form className="relative w-full mt-6 space-y-8">
+						<div className="relative">
+							<label className="absolute px-2 ml-2 -mt-3 font-medium text-gray-600 bg-white">Email Address</label>
+							<input
+								type="text"
+								className="block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-black"
+								placeholder="XxX@email.com"
+							/>
+						</div>
+						<div className="relative">
+							<label className="absolute px-2 ml-2 -mt-3 font-medium text-gray-600 bg-white">Password</label>
+							<input
+								type="text"
+								className="block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-black"
+								placeholder="pa@ssword"
+							/>
+						</div>
+						<div className="relative">
+							<button className="inline-block w-full px-5 py-4 text-xl font-medium text-center text-white transition duration-200 bg-gray-500 rounded-lg hover:bg-gray-900 ease">
+								Submit
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
+export default SignInModal;
--- frontend/src/layout/layout.tsx
+++ frontend/src/layout/layout.tsx
@@ -11,7 +11,6 @@ export default function Layout({ children }: ILayout) {
 		<>
 			<Header />
 			<Sidebar />
-
 			{children}
 		</>
 	);
