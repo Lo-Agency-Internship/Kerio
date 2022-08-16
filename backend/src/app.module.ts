9d66c4b6a8a08d455d6e1a6c83d068f7d8d1b8c3
Hutan Alinaghi <hutan.alinaghi88@gmail.com>
2022-08-16 17:56:08 +0330

    feat: Install @nestjs/typeorm, @nestjs/config and Connect to Database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRootAsync(
+      {
+        imports:[ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService)=>(
+          {
+            type: 'postgres',
+            url: configService.get('DB_URL'),
+            ssl:{rejectUnauthorized: false},
+            synchronize: true,
+            logging: true,
+            entities:[],
+            }
+        )
+      }
+    )
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
