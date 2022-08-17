304adb41385f7a8068bcebd36bb494b497e57500
mahsa abadian <abadian.ma@gmail.com>
2022-08-17 17:17:16 +0430

    feat:add User entity

---
+++ backend/src/entities/user.entity.ts
@@ -0,0 +1,37 @@
+import {
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+  } from 'typeorm';
+
+  @Entity()
+export class User {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
+  
+// @OneToOne(() => orgUser)
+//     @JoinColumn()
+//     orgUser: orgUser
