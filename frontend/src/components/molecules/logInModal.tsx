82a68caa0ca50e34c08ba876946f5fac601153a1
unknown <maryam.pourganji92@gmail.com>
2022-08-18 10:29:04 +0430

    fix: password type

--- frontend/src/components/molecules/logInModal.tsx
+++ frontend/src/components/molecules/logInModal.tsx
@@ -41,7 +41,7 @@ function SignInModal({ setOpen }: any) {
 						<div className="relative">
 							<label className="absolute px-2 ml-2 -mt-3 font-medium text-gray-600 bg-white">Password</label>
 							<input
-								type="text"
+								type="password"
 								className="block w-full px-4 py-3 mt-2 text-base placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-black"
 								placeholder="pa@ssword"
 							/>
