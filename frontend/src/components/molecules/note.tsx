6e1e33f0912e701342378a3692b405a23e31d106
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-08-22 18:06:22 +0430

    feat/creat note

--- frontend/src/components/molecules/note.tsx
+++ frontend/src/components/molecules/note.tsx
@@ -1,8 +1,11 @@
+import NoteDropDown from '../molecules/noteDropDown';
+
 export default function Note() {
 	return (
 		<>
 			<div className="flex justify-evenly w-7/12 ">
 				<h1>Note</h1>;
+				<NoteDropDown />
 			</div>
 		</>
 	);
--- frontend/src/components/molecules/noteDropDown.tsx
+++ frontend/src/components/molecules/noteDropDown.tsx
@@ -1,108 +1,44 @@
 export default function NoteDropDown() {
 	return (
 		<>
-			{/*         
-			<style>
-            .tab {
-            overflow: hidden;
-            }
-            .tab-content {
-            max-height: 0;
-            transition: all 0.5s;
-            }
-            input:checked + .tab-label .test {
-                background-color: #000;
-            }
-            input:checked + .tab-label .test svg {
-                transform: rotate(180deg);
-                stroke: #fff;
-            }
-            input:checked + .tab-label::after {
-            transform: rotate(90deg);
-            }
-            input:checked ~ .tab-content {
-            max-height: 100vh;
-            }
-        </style> */}
+			<form>
+				<div className="md:px-20 pt-1">
+					<div className=" bg-white rounded-md px-6 py-10 max-w-2xl mx-auto">
+						<h1 className="text-center text-2xl font-bold text-gray-500 mb-10">ADD Note</h1>
 
-			<main className="w-3/5 p-8 mx-auto">
-				<section className="shadow row">
-					<div className="tabs">
-						<div className="border-b tab">
-							<div className="border-b tab">
-								<div className="border-l-2 border-transparent relative">
-									<input
-										name="name"
-										id="title"
-										className="w-full absolute z-10 cursor-pointer opacity-0 h-5 top-6"
-										type="checkbox"
-									/>
-									<header className="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none tab-label">
-										<span className="text-grey-darkest font-thin text-xl">This is title of notes</span>
-
-										<div className="rounded-full border border-grey w-7 h-7 flex items-center justify-center test">
-											{/* <!-- icon by feathericons.com --> */}
-											<svg
-												aria-hidden="true"
-												className=""
-												data-reactid="266"
-												fill="none"
-												height="24"
-												stroke="#606F7B"
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
-												width="24"
-												xmlns="http://www.w3.org/2000/svg">
-												<polyline points="6 9 12 15 18 9"></polyline>
-											</svg>
-										</div>
-									</header>
-									<div className="tab-content">
-										<div className="pl-8 pr-8 pb-5 text-grey-darkest">
-											<div className="flex items-center p-4 bg-white border-2 border-gray-200 rounded-lg shadow-sm dark:bg-gray-800">
-												<div className="p-1 mr-8 bg-blue-900 text-white rounded-full">
-													<img
-														className="flex items-baseline justify-self-center "
-														src="https://img.icons8.com/plasticine/100/000000/note.png"
-													/>
-												</div>
-												<div>
-													<div className="container py-5 max-w-md mx-auto">
-														<form method="" action="" />
-
-														<div className="mb-5">
-															<input
-																name="name"
-																id="title"
-																placeholder="Description"
-																className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-																type="text"
-															/>
-														</div>
-													</div>
-												</div>
-											</div>
-											<div className="flex items-center justify-around pt-8">
-												<button
-													className="bg-blue-300 hover:bg-blue-500 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
-													type="button">
-													save
-												</button>
-												<button
-													className="bg-blue-300 hover:bg-blue-500 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
-													type="button">
-													edit
-												</button>
-											</div>
-										</div>
-									</div>
-								</div>
+						<div className="space-y-4">
+							<div>
+								<label className="text-lx font-serif">Title:</label>
+								<input
+									type="text"
+									placeholder="title"
+									id="title"
+									className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md"
+								/>
+							</div>
+							<div>
+								<label className="block mb-2 text-lg font-serif">Description:</label>
+								<textarea
+									id="description"
+									placeholder="write here..."
+									className="w-full font-serif  p-4 text-gray-600 bg-indigo-50 outline-none rounded-md"></textarea>
+							</div>
+							<div className="flex items-center justify-around pt-8">
+								<button
+									className="bg-indigo-600 hover:bg-blue-500 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
+									type="button">
+									save
+								</button>
+								<button
+									className="bg-indigo-600 hover:bg-blue-500 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
+									type="button">
+									edit
+								</button>
 							</div>
 						</div>
 					</div>
-				</section>
-			</main>
+				</div>
+			</form>
 		</>
 	);
 }
