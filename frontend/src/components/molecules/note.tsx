27e6528c642ee04258292c5a9b20b0e037c7e58f
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-08-23 16:10:00 +0430

    fix/location of modal

--- frontend/src/components/molecules/note.tsx
+++ frontend/src/components/molecules/note.tsx
@@ -1,17 +1,8 @@
-import { useState } from 'react';
-import NoteModal from './noteModal';
-// import NoteModal from '../components/molecules/noteModal';
-
-export default function Note() {
-	const [showNoteModal, setShowNoteModal] = useState(false);
+export default function Note({ setShowNoteModal }: any) {
 	return (
 		<>
-			<div className="flex justify-evenly w-11/12 ">
-				<NoteModal />
-				{/* // <!-- This is button of Note --> */}
-				{showNoteModal && <NoteModal setOpen={setShowNoteModal} />}
-
-				<div className="m-3 pt-16 py-8 px-8">
+			<div className="flex justify-center align-item item-center w-11/12 ">
+				<div className="m-8 py-1 px-1">
 					<button
 						onClick={() => setShowNoteModal(true)}
 						className="w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
--- frontend/src/components/molecules/noteModal.tsx
+++ frontend/src/components/molecules/noteModal.tsx
@@ -1,61 +1,67 @@
 export default function NoteModal({ setOpen }: any) {
 	return (
 		<>
-			<form>
-				<div className="absolute pt-1 my-6 mx-30">
-					<div className=" bg-white rounded-md px-6 py-10 max-w-2xl mx-auto">
-						<button
-							className="cursor-pointer absolute top-0 right-0 mt-8 mr-8
+			<div className="pb-9 z-10 ">
+				<form>
+					<div className="absolute flex top-16 right-16 mt-8 mr-16 my-5">
+						<div className=" bg-white rounded-md px-6 py-10 max-w-2xl mx-auto">
+							<button
+								className="cursor-pointer absolute top-0 right-0 mt-4 mr-4
                          text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out 
                          rounded focus:ring-2 focus:outline-none focus:ring-gray-600"
-							onClick={() => setOpen(false)}
-							aria-label="close modal"
-							role="button">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="icon icon-tabler icon-tabler-x"
-								width="20"
-								height="20"
-								viewBox="0 0 24 24"
-								strokeWidth="2.5"
-								stroke="currentColor"
-								fill="none"
-								strokeLinecap="round"
-								strokeLinejoin="round">
-								<path stroke="none" d="M0 0h24v24H0z" />
-								<line x1="18" y1="6" x2="6" y2="18" />
-								<line x1="6" y1="6" x2="18" y2="18" />
-							</svg>
-						</button>
-						<h1 className="text-center text-2xl font-bold text-gray-500 mb-10">ADD Note</h1>
-						<div className="space-y-4">
-							<div>
-								<label className="text-lx font-serif">Title:</label>
-								<input
-									type="text"
-									placeholder="title"
-									id="title"
-									className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md"
-								/>
-							</div>
-							<div>
-								<label className="block mb-2 text-lg font-serif">Description:</label>
-								<textarea
-									id="description"
-									placeholder="write here..."
-									className="w-full font-serif  p-4 text-gray-600 bg-indigo-50 outline-none rounded-md"></textarea>
-							</div>
-							<div className="flex items-center justify-around pt-8">
-								<button
-									className="bg-indigo-600 hover:bg-blue-500 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
-									type="button">
-									save
-								</button>
+								onClick={() => setOpen(false)}
+								aria-label="close modal"
+								role="button">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="icon icon-tabler icon-tabler-x"
+									width="20"
+									height="20"
+									viewBox="0 0 24 24"
+									strokeWidth="2.5"
+									stroke="currentColor"
+									fill="none"
+									strokeLinecap="round"
+									strokeLinejoin="round">
+									<path stroke="none" d="M0 0h24v24H0z" />
+									<line x1="18" y1="6" x2="6" y2="18" />
+									<line x1="6" y1="6" x2="18" y2="18" />
+								</svg>
+							</button>
+							<h1 className="text-center text-2xl font-bold text-gray-500 mb-10">The Form of Notes</h1>
+							<div className="space-y-4">
+								<div>
+									<label className="text-lx font-normal">Date:</label>
+									<input type="date" id="date" className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md" />
+								</div>
+								<div>
+									<label className="text-lx font-normal">Title:</label>
+									<input
+										type="text"
+										placeholder="title"
+										id="title"
+										className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md"
+									/>
+								</div>
+								<div>
+									<label className="block mb-2 text-lg font-normal">Description:</label>
+									<textarea
+										id="description"
+										placeholder="write here..."
+										className="w-full font-serif  p-4 text-gray-600 bg-indigo-50 outline-none rounded-md"></textarea>
+								</div>
+								<div className="flex items-center justify-around pt-8">
+									<button
+										className="bg-indigo-600 hover:bg-blue-500 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
+										type="button">
+										save
+									</button>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</form>
+				</form>
+			</div>
 		</>
 	);
 }
