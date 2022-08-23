cd224b7a54f348d8a00339774a54341b741988c3
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-08-23 13:06:55 +0430

    fix/noteModal

--- frontend/src/components/molecules/note.tsx
+++ frontend/src/components/molecules/note.tsx
@@ -1,11 +1,23 @@
-import NoteDropDown from '../molecules/noteDropDown';
+import { useState } from 'react';
+import NoteModal from './noteModal';
+// import NoteModal from '../components/molecules/noteModal';
 
 export default function Note() {
+	const [showNoteModal, setShowNoteModal] = useState(false);
 	return (
 		<>
-			<div className="flex justify-evenly w-7/12 ">
-				<h1>Note</h1>;
-				<NoteDropDown />
+			<div className="flex justify-evenly w-11/12 ">
+				<NoteModal />
+				{/* // <!-- This is button of Note --> */}
+				{showNoteModal && <NoteModal setOpen={setShowNoteModal} />}
+
+				<div className="m-3 pt-16 py-8 px-8">
+					<button
+						onClick={() => setShowNoteModal(true)}
+						className="w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+						<span className="mx-auto">Add Note</span>
+					</button>
+				</div>
 			</div>
 		</>
 	);
--- frontend/src/components/molecules/noteModal.tsx
+++ frontend/src/components/molecules/noteModal.tsx
@@ -1,11 +1,33 @@
-export default function NoteDropDown() {
+export default function NoteModal({ setOpen }: any) {
 	return (
 		<>
 			<form>
-				<div className="md:px-20 pt-1">
+				<div className="absolute pt-1 my-6 mx-30">
 					<div className=" bg-white rounded-md px-6 py-10 max-w-2xl mx-auto">
+						<button
+							className="cursor-pointer absolute top-0 right-0 mt-8 mr-8
+                         text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out 
+                         rounded focus:ring-2 focus:outline-none focus:ring-gray-600"
+							onClick={() => setOpen(false)}
+							aria-label="close modal"
+							role="button">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="icon icon-tabler icon-tabler-x"
+								width="20"
+								height="20"
+								viewBox="0 0 24 24"
+								strokeWidth="2.5"
+								stroke="currentColor"
+								fill="none"
+								strokeLinecap="round"
+								strokeLinejoin="round">
+								<path stroke="none" d="M0 0h24v24H0z" />
+								<line x1="18" y1="6" x2="6" y2="18" />
+								<line x1="6" y1="6" x2="18" y2="18" />
+							</svg>
+						</button>
 						<h1 className="text-center text-2xl font-bold text-gray-500 mb-10">ADD Note</h1>
-
 						<div className="space-y-4">
 							<div>
 								<label className="text-lx font-serif">Title:</label>
@@ -28,11 +50,6 @@ export default function NoteDropDown() {
 									className="bg-indigo-600 hover:bg-blue-500 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
 									type="button">
 									save
-								</button>
-								<button
-									className="bg-indigo-600 hover:bg-blue-500 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
-									type="button">
-									edit
 								</button>
 							</div>
 						</div>
