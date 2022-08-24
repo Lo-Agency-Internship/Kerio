7d24d242ff3e27048034e52676c4ee6b08c24fe6
Mohamadreza Naderi <mhmdreza.naderi2002@gmail.com>
2022-08-25 00:15:48 +0430

    add function for sign out

--- frontend/src/components/routes/publicRoutes.tsx
+++ frontend/src/components/routes/publicRoutes.tsx
@@ -2,11 +2,10 @@ import { Outlet, Navigate } from 'react-router-dom';
 import { useApiContext } from '../../context/api';
 
 const PublicRoutes = () => {
-	const { checkToken } = useApiContext();
-
+	const { checkToken, setUserToken } = useApiContext();
 	const token = localStorage.getItem('access_token');
-	const isValidToken = checkToken(token);
-
+	setUserToken(token);
+	const isValidToken = checkToken();
 	return isValidToken ? <Outlet /> : <Navigate to="/dashboard" />;
 };
 export default PublicRoutes;
