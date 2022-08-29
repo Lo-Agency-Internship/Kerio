6dd8cf2defeae864f931c4cb7cea623023d0c541
samikia <kia.sami8@gmail.com>
2022-08-29 10:56:44 +0430

    feat: adding timeline ui

--- frontend/src/components/molecules/cardTimeline.tsx
+++ frontend/src/components/molecules/cardTimeline.tsx
@@ -2,22 +2,39 @@ import { useState } from 'react';
 
 export interface Card {
 	title: string;
+	index: number;
 }
-const CardTimeline: React.FC<Card> = ({ title }) => {
+const CardTimeline: React.FC<Card> = ({ title, index }) => {
+	console.log(index);
 	return (
 		<>
-			<div className="flex flex-row-reverse md:contents">
-				<div className="bg-gray-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
-					<h3 className="font-semibold text-lg mb-1">{title}</h3>
-					<p className="leading-tight text-justify">Lorem ipsum dolor sit</p>
-				</div>
-				<div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
-					<div className="h-full w-6 flex items-center justify-center">
-						<div className="h-full w-1 bg-gray-800 pointer-events-none"></div>
+			{index % 2 === 0 ? (
+				<div className="flex flex-row-reverse md:contents">
+					<div className="bg-gray-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
+						<h3 className="font-semibold text-lg mb-1">{title}</h3>
+						<p className="leading-tight text-justify">Lorem ipsum dolor sit</p>
 					</div>
-					<div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-gray-500 shadow"></div>
+					<div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
+						<div className="h-full w-6 flex items-center justify-center">
+							<div className="h-full w-1 bg-gray-800 pointer-events-none"></div>
+						</div>
+						<div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-gray-500 shadow"></div>
+					</div>
 				</div>
-			</div>
+			) : (
+				<div className="flex md:contents">
+					<div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+						<div className="h-full w-6 flex items-center justify-center">
+							<div className="h-full w-1 bg-gray-800 pointer-events-none"></div>
+						</div>
+						<div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-gray-500 shadow"></div>
+					</div>
+					<div className="bg-gray-500 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md">
+						<h3 className="font-semibold text-lg mb-1">{title}</h3>
+						<p className="leading-tight text-justify">Lorem ipsum, dolor sit amet</p>
+					</div>
+				</div>
+			)}
 		</>
 	);
 };
--- frontend/src/components/molecules/timeline.tsx
+++ frontend/src/components/molecules/timeline.tsx
@@ -2,10 +2,22 @@ import { useState } from 'react';
 import CardTimeline from './cardTimeline';
 
 export default function Timeline() {
-	const [timeline, setTimline] = useState([]);
+	// const [timeline, setTimline] = useState([]);
 	const data = [
 		{
 			title: 'add a contact',
+		},
+		{
+			title: 'call a contact',
+		},
+		{
+			title: 'call a contact',
+		},
+		{
+			title: 'call a contact',
+		},
+		{
+			title: 'call a contact',
 		},
 		{
 			title: 'call a contact',
@@ -19,22 +31,10 @@ export default function Timeline() {
 					{/* <!-- left --> */}
 					{/* {Card.map((Element,Index)=>(<CardTimeline Card={Element} key={Index} />))} */}
 					{data.map((element, index) => (
-						<CardTimeline title="call" key={index} />
+						<CardTimeline title={element.title} index={index} />
 					))}
 
 					{/* <!-- right --> */}
-					<div className="flex md:contents">
-						<div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-							<div className="h-full w-6 flex items-center justify-center">
-								<div className="h-full w-1 bg-gray-800 pointer-events-none"></div>
-							</div>
-							<div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-gray-500 shadow"></div>
-						</div>
-						<div className="bg-gray-500 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md">
-							<h3 className="font-semibold text-lg mb-1">Lorem ipsum</h3>
-							<p className="leading-tight text-justify">Lorem ipsum, dolor sit amet</p>
-						</div>
-					</div>
 				</div>
 			</div>
 		</>
