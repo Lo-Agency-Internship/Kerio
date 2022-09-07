e4cb93d6ea8b0e7ba50634ec15c75d95376d1c6f
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-09-07 12:18:57 +0430

    feat:get note data

--- frontend/src/pages/contact.tsx
+++ frontend/src/pages/contact.tsx
@@ -27,14 +27,17 @@ export default function Contact() {
 						<h1 className="pt-2 pl-4 font-bold">{contact?.name} Informations</h1>
 					</div>
 				</div>
-				<div className="flex justify-center">{showNoteModal && <NoteModal setOpen={setShowNoteModal} />}</div>
+				<div className="flex justify-center">
+					{showNoteModal && <NoteModal setOpen={setShowNoteModal} user={contact} setUser={setContact} />}
+				</div>
 				<Profile setUser={setContact} user={contact} />
 
 				<div className="flex justify-center w-12/12 border">
 					<Timeline />
-					<Note setShowNoteModal={setShowNoteModal} />
+					<Note setShowNoteModal={setShowNoteModal} user={contact} setUser={setContact} />
 				</div>
 			</div>
+		
 		</>
 	);
 }
