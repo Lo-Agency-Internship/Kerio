7e7f017faff6b6a9979411ff1aba872b08ad78c2
samikia <kia.sami8@gmail.com>
2022-09-12 20:39:18 +0430

    fix: refine validations

--- frontend/src/validation/addNoteModalValidation.ts
+++ frontend/src/validation/addNoteModalValidation.ts
@@ -2,6 +2,14 @@ import * as yup from 'yup';
 
 export const addNoteModalValidation = yup.object().shape({
 	date: yup.date().required('Date is required').typeError('Date is not valid'),
-	title: yup.string().required('Title is required').typeError('Title is not valid'),
-	description: yup.string().required('Description is required').typeError('Description is required'),
+	title: yup
+		.string()
+		.matches(/[A-Z,a-z]/, 'Name should be valid')
+		.required('Title is required')
+		.typeError('Title is not valid'),
+	description: yup
+		.string()
+		.matches(/[A-Z,a-z]/, 'Name should be valid')
+		.required('Description is required')
+		.typeError('Description is required'),
 });
