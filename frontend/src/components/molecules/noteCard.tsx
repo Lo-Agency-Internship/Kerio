2ca9f005d967dc0badc1c7fdb5eb682362c53bd3
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-09-13 21:10:05 +0430

    feat: date has been changed

--- frontend/src/components/molecules/noteCard.tsx
+++ frontend/src/components/molecules/noteCard.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import ShowNoteModal from '../molecules/showNoteModal';
 import { INote } from '../../utils/interfaces/user/note.interface';
+import { format } from 'date-fns';
 interface INoteCards {
 	data: INote;
 }
 const NoteCard: React.FC<INoteCards> = ({ data }) => {
 	const [showNoteModal, setShowNoteModal] = useState(false);
 	const [note, setNote] = useState<INote>(data);
+	const truncate = (input: string) => (input?.length > 8 ? `${input.substring(0, 8)}...` : input);
 
 	return (
 		<>
@@ -19,14 +21,15 @@ const NoteCard: React.FC<INoteCards> = ({ data }) => {
 							<h1 className="text-lg font-mono text-gray-700">{note.title}</h1>
 						</div>
 					</header>
-					<div className="flex flex-wrap p-2 w-full gap-9 space-x-28">
-						<div className="flex flex-col">
-							<h1 className="text-xs">Date</h1>
-							<h4 className="text-md">{note.date}</h4>
+					<div className="flex p-2 w-full gap-9 space-x-20">
+						<div className="flex">
+							<h1 className="text-xs text-gray-800 font-bold leading-tight tracking-normal">Date: </h1>
+							<h4 className="text-sm">{note.date && format(new Date(note.date), ' dd/mm/yyyy')}</h4>
 						</div>
 						<div className="flex flex-col">
-							<h1 className="text-xs">Description</h1>
-							<h4 className="text-md font-thin">{note.description}</h4>
+							<h1 className="text-xs text-gray-800 font-bold leading-tight tracking-normal">Description: </h1>
+							{/* {truncate(this.state.banners.overview)}							 */}
+							<h4 className="text-xs font-thin">{note.description}</h4>
 						</div>
 					</div>
 				</div>
