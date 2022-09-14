755ae135cabdc1e0b513d6dff949c95ba2016e41
unknown <maryam.pourganji92@gmail.com>
2022-09-14 15:43:23 +0430

    feat: filter types of contacts

--- frontend/src/components/organisms/sidebar.tsx
+++ frontend/src/components/organisms/sidebar.tsx
@@ -1,11 +1,74 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from '../atoms/button';
 import ContactModal from '../molecules/contactModal';
 import AddEmployModal from '../molecules/addEmployModal';
 import Images from '../../assets/images/logo.png';
+import { uri } from '../../utils';
+import axios from 'axios';
+import { useApiContext } from '../../context/api';
 const Sidebar = () => {
 	const [showContactModal, setShowContactModal] = useState<boolean>(false);
 	const [showAddEmployModal, setAddEmployModal] = useState<boolean>(false);
+	const { contacts, setContacts } = useApiContext();
+
+	const allContacts = async () => {
+		const leads = await axios.get(uri(`contacts`), {
+			headers: {
+				Authorization: ` Bearer ${localStorage.getItem('access_token')}`,
+			},
+		});
+		setContacts(leads.data);
+	};
+	const filterLead = async () => {
+		const leads = await axios.get(uri(`contacts?status=Lead`), {
+			headers: {
+				Authorization: ` Bearer ${localStorage.getItem('access_token')}`,
+			},
+		});
+		setContacts(leads.data);
+	};
+	const filterLostLoyal = async () => {
+		const leads = await axios.get(uri(`contacts?status=Lostloyal`), {
+			headers: {
+				Authorization: ` Bearer ${localStorage.getItem('access_token')}`,
+			},
+		});
+		setContacts(leads.data);
+	};
+
+	const potentialCustomer = async () => {
+		const leads = await axios.get(uri(`contacts?status=PotentialCustomer`), {
+			headers: {
+				Authorization: ` Bearer ${localStorage.getItem('access_token')}`,
+			},
+		});
+		setContacts(leads.data);
+	};
+
+	const lostPotentialCustomer = async () => {
+		const leads = await axios.get(uri(`contacts?status=LostPotentialCustomer`), {
+			headers: {
+				Authorization: ` Bearer ${localStorage.getItem('access_token')}`,
+			},
+		});
+		setContacts(leads.data);
+	};
+	const loyalCustomer = async () => {
+		const leads = await axios.get(uri(`contacts?status=LoyalCustomer`), {
+			headers: {
+				Authorization: ` Bearer ${localStorage.getItem('access_token')}`,
+			},
+		});
+		setContacts(leads.data);
+	};
+	const lostLoyalCustomer = async () => {
+		const leads = await axios.get(uri(`contacts?status=LostLoyalCustomer`), {
+			headers: {
+				Authorization: ` Bearer ${localStorage.getItem('access_token')}`,
+			},
+		});
+		setContacts(leads.data);
+	};
 	return (
 		<>
 			{showAddEmployModal && <AddEmployModal setOpen={setAddEmployModal} />}
@@ -37,37 +100,57 @@ const Sidebar = () => {
 					<li className="relative">
 						<Button
 							style="border-black  hover:text-gray-500 font-bold text-center text-white px-2 mx-2 mt-6"
+							label={'All Contacts'}
+							onClick={allContacts}
+							type="button"
+						/>
+					</li>
+					<li className="relative">
+						<Button
+							style="border-black  hover:text-gray-500 font-bold text-center text-white px-2 mx-2 mt-6"
 							label={'Leads'}
+							onClick={filterLead}
+							type="button"
 						/>
 					</li>
 					<li className="relative">
 						<Button
 							style=" border-black  hover:text-gray-500 font-bold text-center text-white px-2 m-2"
 							label={'Lost Loyal'}
+							onClick={filterLostLoyal}
+							type="button"
 						/>
 					</li>
 					<li className="relative">
 						<Button
 							style="  border-black  hover:text-gray-500 font-bold text-center text-white px-2 m-2"
 							label={'Potential Customer'}
+							onClick={potentialCustomer}
+							type="button"
 						/>
 					</li>
 					<li className="relative">
 						<Button
 							style="border-black  hover:text-gray-500 font-bold text-center text-white px-2 m-2"
 							label={'Lost Potential Customer'}
+							onClick={lostPotentialCustomer}
+							type="button"
 						/>
 					</li>
 					<li className="relative">
 						<Button
 							style="border-black  hover:text-gray-500 font-bold text-center text-white px-2 m-2"
 							label={'Loyal Customer'}
+							onClick={loyalCustomer}
+							type="button"
 						/>
 					</li>
 					<li className="relative">
 						<Button
 							style="border-black  hover:text-gray-500 font-bold text-center text-white px-2 m-2"
 							label={'Lost Loyal Customer'}
+							onClick={lostLoyalCustomer}
+							type="button"
 						/>
 					</li>
 				</ul>
