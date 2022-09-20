0504ac65bd78331e3a6b51c9cded47d3da46be4c
unknown <maryam.pourganji92@gmail.com>
2022-09-20 14:27:55 +0430

    feat: Add data table for customer information

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -2,19 +2,14 @@ import { useApiContext } from '../context/api';
 import { useEffect } from 'react';
 import ContactCards from '../components/molecules/contactCard';
 import Loading from '../components/molecules/loading';
+import DataTable from 'react-data-table-component';
+import ContactTable from '../components/organisms/contactTable';
 
 export default function Dashboard() {
 	const { getAllContacts, change, isLoading, contacts, setContacts } = useApiContext();
 	useEffect(() => {
 		getAllContacts().then(setContacts);
 	}, [change]);
-	return (
-		<>
-			{isLoading ? (
-				<Loading />
-			) : (
-				contacts.map((element: any, index: any) => <ContactCards contact={element} key={index} />)
-			)}
-		</>
-	);
+
+	return <>{isLoading ? <Loading /> : <ContactTable contact={contacts} />}</>;
 }
