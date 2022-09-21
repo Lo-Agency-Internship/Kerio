dc8465b965a6a222ddc60f71363df2bd57c7fbe0
unknown <maryam.pourganji92@gmail.com>
2022-09-21 13:00:46 +0430

    feat: pagination for server side

--- frontend/src/components/organisms/contactTable.tsx
+++ frontend/src/components/organisms/contactTable.tsx
@@ -1,3 +1,4 @@
+import { SetStateAction, useEffect, useState } from 'react';
 import DataTable, { TableColumn } from 'react-data-table-component';
 import { IUser } from '../../utils/interfaces/user';
 interface IContactTable {
@@ -31,7 +32,59 @@ const columns: TableColumn<IUser>[] = [
 	},
 ];
 const ContactTable: React.FC<IContactTable> = ({ contact }) => {
-	return <DataTable columns={columns} data={contact} pagination />;
-};
+	const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [totalRows, setTotalRows] = useState(0);
+  const [perPage, setPerPage] = useState(10);
+
+  useEffect(() => {
+    fetchData(1, perPage);
+  }, [perPage])
+
+  const fetchData = async (page: number, per_page: number) => {
+    fetch(`https://www.mecallapi.com/api/attractions?page=${page}&per_page=${per_page}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.data);
+          setTotalRows(result.total);
+        },
+        (error) => {
+          setIsLoaded(true);
+		  setError(error);
+        }
+      )
+  }
+
+  const handlePageChange = (page: any) => {
+    fetchData(page, perPage);
+  }
+
+  const handlePerRowsChange = async (newPerPage: SetStateAction<number>, page: any) => {
+    setPerPage(newPerPage);
+  }
+  if (error) {
+    return <div>{error && <p className="text-red-700">{error}</p>}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+
+	return(
+		<div className="App">
+	 <DataTable columns={columns} 
+	 data={contact} 
+	 pagination
+          paginationServer
+          paginationTotalRows={totalRows}
+          onChangePage={handlePageChange}
+          onChangeRowsPerPage={handlePerRowsChange}
+	 
+	 />
+	 </div>
+	);
+}
+}
 
 export default ContactTable;
