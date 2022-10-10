6f9fff3f2894ccfdb4b311b753824dd1f0a67090
Hutan Alinaghi <hutan.alinaghi88@gmail.com>
2022-10-10 09:29:09 +0330

    feat: create note interfaces

---
+++ backend/src/interfaces/note.service.interface.ts
@@ -0,0 +1,42 @@
+import { IPaginationParams } from './index';
+import { DeepPartial, FindOperator } from 'typeorm';
+import { Note } from 'src/entities/note.entity';
+
+
+export interface IFindPayload extends IPaginationParams {
+  organizationId: number | FindOperator<number>;
+ 
+}
+
+export interface IFindOneByIdPayload {
+  organizationId: number | FindOperator<number>;
+  id: number;
+}
+
+export interface ICreatePayload {
+  organizationId: number;
+  note: DeepPartial<Note>;
+}
+
+export interface IUpdateOneByIdPayload {
+  id: number;
+  note: DeepPartial<Note>;
+}
+
+export interface IUpdateStatusPayload {
+  id: number;
+  organizationId: number | FindOperator<number>;
+}
+
+export interface IDeletePayload {
+  id: number;
+}
+
+export interface IPaginatedNotes {
+  notes: Note[];
+  metadata: {
+    total: number;
+    size: number;
+    page: number;
+  };
+}
