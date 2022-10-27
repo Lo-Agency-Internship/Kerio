10757f9e24a65f1e0d652d89eae9c9251a03b4f6
samikia <kia.sami8@gmail.com>
2022-10-27 13:02:00 +0330

    fix: delete and add some codes

--- frontend/src/pages/contacts/account.tsx
+++ frontend/src/pages/contacts/account.tsx
@@ -47,6 +47,14 @@ export const Account: React.FC<AccountProps> = ({ user, setUser }) => {
 												</svg>
 											),
 										},
+										{
+											label: 'Roadmap',
+											icon: () => (
+												<svg className={`w-4 h-4 shrink-0 fill-current text-slate-400 mr-2`} viewBox="0 0 16 16">
+													<path d="M5 9h11v2H5V9zM0 9h3v2H0V9zm5 4h6v2H5v-2zm-5 0h3v2H0v-2zm5-8h7v2H5V5zM0 5h3v2H0V5zm5-4h11v2H5V1zM0 1h3v2H0V1z" />
+												</svg>
+											),
+										},
 									]}
 								/>
 								{sidebarActiveItem === 'account' && (
