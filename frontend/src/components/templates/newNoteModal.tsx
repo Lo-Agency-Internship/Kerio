de959c35749c29ad03a6500e18f28450ff9359ed
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-11-14 10:18:51 +0330

    feat: toast in note

--- frontend/src/components/templates/newNoteModal.tsx
+++ frontend/src/components/templates/newNoteModal.tsx
@@ -5,7 +5,8 @@ import { useParams } from 'react-router-dom';
 import Modal from '../organisms/modal';
 import { InputFormControl } from '../molecules/formControls/inputFormControl';
 import { SelectFormControl } from '../molecules/formControls/selectFormControl';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 interface IProps {
 	setOpen: (close: boolean) => void;
 	open: boolean;
@@ -46,94 +47,128 @@ const NewNoteModal: FC<IProps> = ({ setOpen, open }) => {
 			});
 			setOpen(false);
 			setChange(!change);
+			toast.success('Look! You have a new note in your profile!', {
+				position: 'top-center',
+				autoClose: 5000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+				theme: 'light',
+			});
 		} catch (err: any) {
 			setError(err.response.data.message);
+			toast.error('Something went wrong! ', {
+				position: 'top-right',
+				autoClose: 8000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+				theme: 'light',
+			});
 		}
 	};
 
 	return (
-		<Modal
-			show={open}
-			onClose={() => setOpen(false)}
-			title={'New Note'}
-			actions={[
-				{
-					label: 'Submit',
-					type: 'submit',
-					form: ADDNOTE_FORM_ID,
-				},
-			]}>
-			{error && <p>{error}</p>}
-			<form id={ADDNOTE_FORM_ID} onSubmit={handleSubmit} className="relative w-full mt-6 space-y-8">
-				<InputFormControl
-					label={'Title'}
-					inputProps={{
-						type: 'text',
-						placeholder: 'Title of your note',
-					}}
-				/>
-				<InputFormControl
-					label={'Date'}
-					inputProps={{
-						type: 'date',
-						placeholder: 'Date',
-					}}
-				/>
-				<InputFormControl
-					label={'Description'}
-					inputProps={{
-						type: 'text',
-						placeholder: 'Description',
-					}}
-				/>
-				<SelectFormControl
-					label={'Score'}
-					iSelectProps={{
-						iOptionProps: [
-							{
-								title: '+1',
-								value: '+1',
-							},
-							{
-								title: '-1',
-								value: '-1',
-							},
-							{
-								title: '0',
-								value: '0',
-							},
-						],
-					}}
-				/>
-				<SelectFormControl
-					label={'Status'}
-					iSelectProps={{
-						iOptionProps: [
-							{
-								title: 'Lead',
-								value: 'Lead',
-							},
-							{
-								title: 'PotentialCustomer',
-								value: 'PotentialCustomer',
-							},
-							{
-								title: 'LostPotentialCustomer',
-								value: 'LostPotentialCustomer',
-							},
-							{
-								title: 'LostLoyalCustomer',
-								value: 'LostLoyalCustomer',
-							},
-							{
-								title: 'LoyalCustomer',
-								value: 'LoyalCustomer',
-							},
-						],
-					}}
-				/>
-			</form>
-		</Modal>
+		<>
+			<Modal
+				show={open}
+				onClose={() => setOpen(false)}
+				title={'New Note'}
+				actions={[
+					{
+						label: 'Submit',
+						type: 'submit',
+						form: ADDNOTE_FORM_ID,
+					},
+				]}>
+				{error && <p>{error}</p>}
+				<form id={ADDNOTE_FORM_ID} onSubmit={handleSubmit} className="relative w-full mt-6 space-y-8">
+					<InputFormControl
+						label={'Title'}
+						inputProps={{
+							type: 'text',
+							placeholder: 'Title of your note',
+						}}
+					/>
+					<InputFormControl
+						label={'Date'}
+						inputProps={{
+							type: 'date',
+							placeholder: 'Date',
+						}}
+					/>
+					<InputFormControl
+						label={'Description'}
+						inputProps={{
+							type: 'text',
+							placeholder: 'Description',
+						}}
+					/>
+					<SelectFormControl
+						label={'Score'}
+						iSelectProps={{
+							iOptionProps: [
+								{
+									title: '+1',
+									value: '+1',
+								},
+								{
+									title: '-1',
+									value: '-1',
+								},
+								{
+									title: '0',
+									value: '0',
+								},
+							],
+						}}
+					/>
+					<SelectFormControl
+						label={'Status'}
+						iSelectProps={{
+							iOptionProps: [
+								{
+									title: 'Lead',
+									value: 'Lead',
+								},
+								{
+									title: 'PotentialCustomer',
+									value: 'PotentialCustomer',
+								},
+								{
+									title: 'LostPotentialCustomer',
+									value: 'LostPotentialCustomer',
+								},
+								{
+									title: 'LostLoyalCustomer',
+									value: 'LostLoyalCustomer',
+								},
+								{
+									title: 'LoyalCustomer',
+									value: 'LoyalCustomer',
+								},
+							],
+						}}
+					/>
+				</form>
+			</Modal>
+			<ToastContainer
+				position="top-right"
+				autoClose={8000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="light"
+			/>
+		</>
 	);
 };
 
