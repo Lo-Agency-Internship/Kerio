98206f6c8e5bc9a08bd82682b6900a795be7f421
Mohamadreza Naderi <mhmdreza.naderi2002@gmail.com>
2022-12-04 14:18:05 +0330

    feat: create 2 functions for handle next and pre page

--- frontend/src/components/organisms/notePannel.tsx
+++ frontend/src/components/organisms/notePannel.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { INote } from '../../utils/interfaces/user/note.interface';
 import { useApiContext } from '../../context/api';
 import EditNoteModal from '../templates/editNoteModal';
+import { Button } from '../atoms/button';
 
 interface IProps {
 	statuses: any;
@@ -17,28 +18,48 @@ const NotePannel: React.FC<IProps> = ({ statuses }) => {
 	const [editNoteModal, setEditNoteModal] = useState<boolean>(false);
 	const [note, setNote] = useState<INote>();
 
-	const getNotes = async () => {
+	const [pageCount, setPageCount] = useState<number>(1);
+	const [sizeCount, setSizeCount] = useState<number>(3);
+	const [isLoadingNextPage, setIsLoadingNextPage] = useState<boolean>(false);
+	const [isLoadingPreviousPage, setIsLoadingPreviousPage] = useState<boolean>(false);
+	const [totalPage, setTotalPage] = useState<number>(1);
+
+	const getNotes = async (page: number, size: number) => {
 		try {
 			const data = await getAllNotes({
 				id,
 				pagination: {
-					page: 1,
-					size: 3,
+					page: page,
+					size: size,
 					sort: 'asc',
 				},
 			});
+			setTotalPage(data.metadata);
 			setNotes(data.notes);
 		} catch (e: any) {
 			setError(e.response.data.message);
 		}
 	};
 	useEffect(() => {
-		getNotes();
+		getNotes(pageCount, sizeCount);
 	}, [change]);
 
-	// const handleNextPage = () => {};
+	let number = pageCount;
+	const handleNextPage = () => {
+		setIsLoadingNextPage(true);
+		++number;
+		setPageCount(number);
+		getNotes(pageCount + 1, sizeCount);
+		setIsLoadingNextPage(false);
+	};
 
-	// const handlePreviousPage = () => {};
+	const handlePreviousPage = () => {
+		setIsLoadingPreviousPage(true);
+		--number;
+		setPageCount(number);
+		getNotes(pageCount - 1, sizeCount);
+		setIsLoadingPreviousPage(false);
+	};
 	return (
 		<>
 			{editNoteModal && (
@@ -60,15 +81,23 @@ const NotePannel: React.FC<IProps> = ({ statuses }) => {
 				<div className="mt-8 text-right">
 					<nav className="inline-flex" role="navigation" aria-label="Navigation">
 						<ul className="flex justify-center my-4">
+							{pageCount > 1 && (
+								<li className="ml-3 first:ml-0">
+									<Button
+										loading={isLoadingPreviousPage}
+										onClick={handlePreviousPage}
+										style="btn bg-gray-100 border-slate-500 hover:border-slate-500 text-indigo-600 text-sm py-1 px-2 rounded-md">
+										Previous
+									</Button>
+								</li>
+							)}
 							<li className="ml-3 first:ml-0">
-								<button className="btn bg-gray-200 border-slate-500 text-slate-500 cursor-not-allowed p-2 rounded-md">
-									Previous
-								</button>
-							</li>
-							<li className="ml-3 first:ml-0">
-								<button className="btn bg-gray-100 border-slate-500 hover:border-slate-500 text-indigo-500 p-2 rounded-md">
+								<Button
+									loading={isLoadingNextPage}
+									onClick={handleNextPage}
+									style="btn bg-gray-100 border-slate-500 hover:border-slate-500 text-indigo-600 text-sm py-1 px-2 rounded-md">
 									Next
-								</button>
+								</Button>
 							</li>
 						</ul>
 					</nav>
