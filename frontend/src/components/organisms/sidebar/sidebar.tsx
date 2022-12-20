d8ee1828b2fe0adf7f5c60deb4ee36118bf6bc0b
Mohamadreza Naderi <mhmdreza.naderi2002@gmail.com>
2022-12-20 13:56:54 +0330

    fix: resolve problem in sidebar

--- frontend/src/components/organisms/sidebar/sidebar.tsx
+++ frontend/src/components/organisms/sidebar/sidebar.tsx
@@ -1,21 +1,25 @@
 import * as React from 'react';
-import { NavLink } from 'react-router-dom';
 
 export interface SidebarProps {
 	children: React.ReactNode;
-	sidebarOpen?: boolean;
-	setSidebarOpen: any;
+	sidebarOpen: boolean;
+	setSidebarOpen: (newState: boolean) => void;
+	sidebarExpanded: boolean;
+	setSidebarExpanded: (newState: boolean) => void;
 	logo?: React.ReactNode;
+	onLogoClicked?(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void;
 }
-export const Sidebar: React.FC<SidebarProps> = ({ children, sidebarOpen, setSidebarOpen }) => {
+export const Sidebar: React.FC<SidebarProps> = ({
+	children,
+	sidebarOpen,
+	logo,
+	onLogoClicked,
+	setSidebarOpen,
+	sidebarExpanded,
+	setSidebarExpanded,
+}) => {
 	const trigger = React.useRef<any>(null);
 	const sidebar = React.useRef<any>(null);
-
-	const storedSidebarExpanded = localStorage.getItem('sidebar-expanded');
-	const [sidebarExpanded, setSidebarExpanded] = React.useState(
-		storedSidebarExpanded === null ? false : storedSidebarExpanded === 'true',
-	);
-
 	// close on click outside
 	React.useEffect(() => {
 		const clickHandler = ({ target }: any) => {
@@ -26,7 +30,6 @@ export const Sidebar: React.FC<SidebarProps> = ({ children, sidebarOpen, setSide
 		document.addEventListener('click', clickHandler);
 		return () => document.removeEventListener('click', clickHandler);
 	});
-
 	// close if the esc key is pressed
 	React.useEffect(() => {
 		const keyHandler = ({ keyCode }: any) => {
@@ -36,15 +39,6 @@ export const Sidebar: React.FC<SidebarProps> = ({ children, sidebarOpen, setSide
 		document.addEventListener('keydown', keyHandler);
 		return () => document.removeEventListener('keydown', keyHandler);
 	});
-
-	React.useEffect(() => {
-		localStorage.setItem('sidebar-expanded', `${sidebarExpanded}`);
-		if (sidebarExpanded) {
-			document.querySelector('body')?.classList.add('sidebar-expanded');
-		} else {
-			document.querySelector('body')?.classList.remove('sidebar-expanded');
-		}
-	}, [sidebarExpanded]);
 
 	return (
 		<div>
@@ -59,67 +53,44 @@ export const Sidebar: React.FC<SidebarProps> = ({ children, sidebarOpen, setSide
 			<div
 				id="sidebar"
 				ref={sidebar}
-				className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out ${
-					sidebarOpen ? 'translate-x-0' : '-translate-x-64'
-				}`}>
+				className={` flex  flex-col  absolute  z-40 ltr:left-0 rtl:right-0  top-0 lg:static ltr:lg:left-auto rtl:lg:right-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto  no-scrollbar w-64 lg:w-20 2xl:!w-64  shrink-0  bg-slate-800  p-4  transition-all  duration-200  ease-in-out ${
+					sidebarOpen ? 'translate-x-0' : 'ltr:-translate-x-64 rtl:translate-x-64'
+				}
+        ${sidebarExpanded && 'lg:!w-64'}`}>
 				{/* Sidebar header */}
-				<div className="flex justify-between mb-10 pr-3 sm:px-2">
+				<div className="flex justify-between mb-10 ltr:pr-3 rtl:pl-3">
 					{/* Close button */}
 					<button
 						ref={trigger}
-						className="lg:hidden text-slate-500 hover:text-slate-400"
-						onClick={() => setSidebarOpen(!sidebarOpen)}
-						aria-controls="sidebar"
-						aria-expanded={sidebarOpen}>
-						<span className="sr-only">Close sidebar</span>
-						<svg className="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						className="lg:hidden text-slate-500 hover:text-slate-400 ltr:rotate-0 rtl:rotate-180"
+						onClick={() => setSidebarOpen(false)}>
+						<span className=" sr-only">Close sidebar</span>
+						<svg className=" w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 							<path d="M10.7 18.7l1.4-1.4L7.8 13H20v-2H7.8l4.3-4.3-1.4-1.4L4 12z" />
 						</svg>
 					</button>
 					{/* Logo */}
-					<NavLink end to="/" className="block">
-						<svg width="32" height="32" viewBox="0 0 32 32">
-							<defs>
-								<linearGradient x1="28.538%" y1="20.229%" x2="100%" y2="108.156%" id="logo-a">
-									<stop stopColor="#A5B4FC" stopOpacity="0" offset="0%" />
-									<stop stopColor="#A5B4FC" offset="100%" />
-								</linearGradient>
-								<linearGradient x1="88.638%" y1="29.267%" x2="22.42%" y2="100%" id="logo-b">
-									<stop stopColor="#38BDF8" stopOpacity="0" offset="0%" />
-									<stop stopColor="#38BDF8" offset="100%" />
-								</linearGradient>
-							</defs>
-							<rect fill="#6366F1" width="32" height="32" rx="16" />
-							<path
-								d="M18.277.16C26.035 1.267 32 7.938 32 16c0 8.837-7.163 16-16 16a15.937 15.937 0 01-10.426-3.863L18.277.161z"
-								fill="#4F46E5"
-							/>
-							<path
-								d="M7.404 2.503l18.339 26.19A15.93 15.93 0 0116 32C7.163 32 0 24.837 0 16 0 10.327 2.952 5.344 7.404 2.503z"
-								fill="url(#logo-a)"
-							/>
-							<path
-								d="M2.223 24.14L29.777 7.86A15.926 15.926 0 0132 16c0 8.837-7.163 16-16 16-5.864 0-10.991-3.154-13.777-7.86z"
-								fill="url(#logo-b)"
-							/>
-						</svg>
-					</NavLink>
+					<a href="#" className=" block" onClick={onLogoClicked}>
+						{logo}
+					</a>
 				</div>
 
 				{/* Links */}
-				<div className="space-y-8">
-					{/* Pages group */}
-					<div>
-						<ul className="mt-3">{children}</ul>
-					</div>
-				</div>
+				<div className="space-y-8">{children}</div>
 				{/* Expand / collapse button */}
 				<div className="pt-3 hidden lg:inline-flex 2xl:hidden justify-end mt-auto">
-					<div className="px-3 py-2">
+					<div className="px-3  py-2">
 						<button onClick={() => setSidebarExpanded(!sidebarExpanded)}>
 							<span className="sr-only">Expand / collapse sidebar</span>
-							<svg className="w-6 h-6 fill-current sidebar-expanded:rotate-180" viewBox="0 0 24 24">
-								<path className="text-slate-400" d="M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z" />
+							<svg
+								className={`w-6 h-6 fill-current ltr:rotate-0 rtl:rotate-180 ${
+									sidebarExpanded && 'ltr:rotate-180 rtl:rotate-0'
+								}`}
+								viewBox="0 0 24 24">
+								<path
+									className=" text-slate-400"
+									d="M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z"
+								/>
 								<path className="text-slate-600" d="M3 23H1V1h2z" />
 							</svg>
 						</button>
