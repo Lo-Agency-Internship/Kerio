8fe244e670235e998bb2a568e9099d769f470ecd
mahsa abadian <abadian.ma@gmail.com>
2023-01-30 10:33:24 +0330

    feat:add test for findUserwithorganizationByusermail

--- backend/src/services/test/organizationUser.Service.spec.ts
+++ backend/src/services/test/organizationUser.Service.spec.ts
@@ -9,6 +9,7 @@ import { DataSource, Repository } from 'typeorm';
 import { OrganizationUserService } from '../organizationUser.service';
 import { Organization } from '../../entities/organization.entity';
 import { ERole } from '../../utils/types';
+import { NotFoundException } from '@nestjs/common';
 
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
@@ -16,7 +17,30 @@ const createMockRepository = <T = any>(): MockRepository<T> => ({
   findOneByOrFail: jest.fn(),
   save: jest.fn(),
   update: jest.fn(),
+  findOne: jest.fn()
 });
+
+const userStub = ()=> {return{
+  user: {
+    id: 1,
+    name: 'mahsa',
+    email: 'goli@d.com',
+    password: '1234556',
+    salt: 'ASE$%RHJJJJJJ',
+    organization: {},
+    createdAt: new Date(),
+  } as User,
+  org: {
+    id: 1,
+    name: 'goli',
+    slug: 'golislug',
+    createdAt: new Date(),
+    contacts: [],
+    OrgUser: {},
+  } as Organization,
+  role: { id: 1, name: ERole.Owner, createdAt: '' } as Role,
+}}
+
 
 describe('organizationUserService', () => {
   let service: OrganizationUserService;
@@ -72,27 +96,7 @@ describe('organizationUserService', () => {
         OrgUser: {},
       } as Organization;
 
-      const expectedResult = {
-        user: {
-          id: 1,
-          name: 'mahsa',
-          email: 'goli@d.com',
-          password: '1234556',
-          salt: 'ASE$%RHJJJJJJ',
-          organization: {},
-          createdAt: new Date(),
-        } as User,
-        org: {
-          id: 1,
-          name: 'goli',
-          slug: 'golislug',
-          createdAt: new Date(),
-          contacts: [],
-          OrgUser: {},
-        } as Organization,
-        role: { id: 1, name: ERole.Owner, createdAt: '' } as Role,
-      };
-
+      const expectedResult = userStub()
       roleRepository.findOneByOrFail.mockResolvedValue({
         id: 1,
         name: 'Owner',
@@ -118,4 +122,66 @@ describe('organizationUserService', () => {
       ).toEqual(expectedResult);
     });
   });
+  describe('findUserWithOrganizationByUserEmail',()=>{
+    it('should return user without salt and pass and with organization Info ',async ()=>{
+      const mockedUser = {
+        id: 2,
+        name: 'tesla',
+        email: 'test@d.com',
+        password: '$2b$10$j.3UXQZBUOS8',
+        salt: '$2b$10$j.3UXQZBUOS8',
+        createdAt: new Date(),
+        organization: {
+          id: 2,
+          createdAt: new Date(),
+          role:  {
+            id: 1,
+            name: 'Owner',
+            createdAt: new Date(),
+            
+          },
+          org: {
+            id: 2,
+            name: "tesla's Organization",
+            address: '',
+            slug: 'hi-teslas-company',
+            createdAt: new Date(),
+          }
+        }
+      }
+
+
+
+      const expectedResult = {
+        id: 2,
+        name: "tesla",
+        email: "test@d.com",
+        createdAt: new Date(),
+        organization: {
+          id: 2,
+          name: "tesla's Organization",
+          address: "",
+          slug: "hi-teslas-company",
+          createdAt: new Date(),
+        },
+        role: {
+          id: 1,
+          name: "Owner",
+          createdAt: new Date(),
+          
+        }
+      }
+    
+
+      
+      userRepository.findOne.mockReturnValue(mockedUser);
+      expect(await service.findUserWithOrganizationByUserEmail('goli@d.com')).toEqual(expectedResult)
+    })
+  });
+  it('should handle error if user does not exists',async()=>{
+    userRepository.findOne.mockResolvedValue(null);
+    expect( service.findUserWithOrganizationByUserEmail('goli@d.com')).rejects.toThrow(
+      NotFoundException,
+    );
+  })
 });
