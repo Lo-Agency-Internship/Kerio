bc0e8b35f71e050af91b69f53b55eaecfc8c66e5
unknown <maryam.pourganji92@gmail.com>
2023-01-30 14:26:31 +0330

    feat: test for updateOneById function

--- backend/src/services/test/contact.service.spec.ts
+++ backend/src/services/test/contact.service.spec.ts
@@ -1,15 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, Repository, UpdateResult } from 'typeorm';
 import { ContactService } from '../contact/contact.service';
 import { SearchService } from '../search.service';
 import { Contact } from '../../entities/contact/contact.entity';
 import { ContactStatus } from '../../entities/contact/contactStatus.entity';
 import { createMock } from '@golevelup/ts-jest';
-
+// export const SearchService = jest.fn().mockReturnValue({
+//   updateDocument: jest.fn()
+// });
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 const createMockRepository = <T = any>(): MockRepository<T> => ({
   findOne: jest.fn(),
+  update: jest.fn(),
 });
 
 const contactStub = () => {
@@ -27,6 +30,7 @@ const contactStub = () => {
 
 describe('contactService', () => {
   let service: ContactService;
+  let searchService;
   let contactRepository: MockRepository;
 
   beforeEach(async () => {
@@ -47,6 +51,7 @@ describe('contactService', () => {
     }).compile();
 
     service = module.get<ContactService>(ContactService);
+    searchService= module.get<SearchService>(SearchService);
     contactRepository = module.get(getRepositoryToken(Contact));
   });
 
@@ -76,6 +81,31 @@ describe('contactService', () => {
           organizationId: 1,
         }),
       ).toBe(null);
+    });
+  });
+  describe('updateOneById', () => {
+    it('should update the contact and return updateresult', async () => {
+      const mockedUpdatedResult: UpdateResult = {
+        raw: 'any',
+        affected: 1,
+        generatedMaps: [],
+      };
+      const mockUpdateMailiSearch={
+        taskUid: 2,
+        indexUid: 'string',
+        status: 'success',
+        type: 'TaskTypes',
+        enqueuedAt: 'string',
+      }
+
+      contactRepository.update.mockResolvedValue(mockedUpdatedResult);
+      expect(await service.updateOneById( {id:1, contact:{name: 'mary'}} )).toEqual({
+        raw: 'any',
+        affected: 1,
+        generatedMaps: [],
+      });
+      expect(searchService.updateDocument).toHaveBeenCalled();
+      searchService.updateDocument.mockResolvedValue(mockUpdateMailiSearch)
     });
   });
 });
