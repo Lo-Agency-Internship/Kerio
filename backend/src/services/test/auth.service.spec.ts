b2ef3b8e1d54a3df51f976e5251f964ebd79652d
unknown <maryam.pourganji92@gmail.com>
2023-02-01 15:13:02 +0330

    fix:lint and format

--- backend/src/services/test/auth.service.spec.ts
+++ backend/src/services/test/auth.service.spec.ts
@@ -15,51 +15,47 @@ jest.mock('../user.service');
 jest.mock('../organization.service');
 jest.mock('../organizationUser.service');
 
-
-
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 const createMockRepository = <T = any>(): MockRepository<T> => ({
   create: jest.fn(),
-  
 });
-describe ('auth.service',()=>{
-    let service: AuthService;
-    let jwtService;
-    beforeEach(async () => {
+describe('auth.service', () => {
+  let service: AuthService;
+  let jwtService;
+  beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-        providers: [
-            UserService,
-            AuthService,
-            OrganizationService,
-            OrganizationUserService,
+      providers: [
+        UserService,
+        AuthService,
+        OrganizationService,
+        OrganizationUserService,
         { provide: DataSource, useValue: {} },
         {
-            provide: JwtService,
-            useValue: createMock<JwtService>(),
-          },
-          { provide: getRepositoryToken(User), useValue: createMockRepository() },
-      
+          provide: JwtService,
+          useValue: createMock<JwtService>(),
+        },
+        { provide: getRepositoryToken(User), useValue: createMockRepository() },
       ],
     }).compile();
-    
+
     service = module.get<AuthService>(AuthService);
-    jwtService=module.get<JwtService>(JwtService);
-    }); 
-    it('should be defined', () => {
-        expect(service).toBeDefined();
-    });
-describe('CreateJwt', () => {
+    jwtService = module.get<JwtService>(JwtService);
+  });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+  describe('CreateJwt', () => {
     it('should return a JWT response object', async () => {
       const mockedUser = {
         id: 1,
-        email:'goli@g.com',
-        name:'Mary',
-        role:{id:1 , name:ERole.Owner} as Role,
+        email: 'goli@g.com',
+        name: 'Mary',
+        role: { id: 1, name: ERole.Owner } as Role,
       } as SecureUserWithOrganization;
-      jwtService.sign.mockResolvedValue('generated_access_token')
-      expect(await service.createJwt(mockedUser)).toEqual(
-        {access_token: 'generated_access_token' }
-      );
+      jwtService.sign.mockResolvedValue('generated_access_token');
+      expect(await service.createJwt(mockedUser)).toEqual({
+        access_token: 'generated_access_token',
+      });
     });
   });
 });
